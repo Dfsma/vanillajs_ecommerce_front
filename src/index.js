11d909b148b9bbc0788efffeabd91482f6fd93d7
Diego <diegof.df58@gmail.com>
2021-01-24 13:29:22 -0300

    ah

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-const baseURL = "https://bsalebackend.herokuapp.com/products"
-  
+const baseURL = "https://bsalebackend.herokuapp.com/products";
 
-  function getProducts() {
-    return fetch("https://bsalebackend.herokuapp.com/products").then(response => {
-        if(!response.ok){
-            throw Error("Error");
-        }
-        return response.json();
-        
-    }).then(data => {
-        console.log(data.products);
-        const html = data.products
-          .map(product => {
-            return `
+function getProducts() {
+  return fetch("https://bsalebackend.herokuapp.com/products")
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("Error");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.products);
+      const html = data.products
+        .map((product) => {
+          return `
             <div class="col-md-4">
               <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="${product.url_image}" alt="${product.name}" style="width:100%; height:30%;">
+                <img class="card-img-top img-fluid" src="${product.url_image}" alt="${product.name}" style="width:100%; height:30%;">
                 <div class="card-body">
                   <h4><b>${product.name}</b></h4>
                   <p>Precio: ${product.price}</p>
@@ -25,17 +25,22 @@ const baseURL = "https://bsalebackend.herokuapp.com/products"
             </div>
             
             `;
-          })
-          .join("");
-        document
-          .querySelector('#app')
-          .innerHTML = (html);
-          
-    }).catch(error => {
-        console.log(error);
+        })
+        .join("");
+      document.querySelector("#app").innerHTML = html;
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  
-  }
+}
+
+function getProductsByCategory() {
+    
+
+}
+
+
 
 
 getProducts();
+getProductsByCategory();

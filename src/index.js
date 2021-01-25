962cc8b2865a3adbaf7fd993ac002e52d589d5dc
Diego <diegof.df58@gmail.com>
2021-01-24 22:25:58 -0300

    addfilterbycategory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const baseURL = "https://bsalebackend.herokuapp.com/products";
+const baseUrlCategories = "https://bsalebackend.herokuapp.com/categories";
+const baseUrlProductsByCategory =
+  "https://bsalebackend.herokuapp.com/categories";
 
 function getProducts() {
-  return fetch("https://bsalebackend.herokuapp.com/products")
+  return fetch(baseURL)
     .then((response) => {
       if (!response.ok) {
         throw Error("Error");
@@ -9,8 +12,8 @@ function getProducts() {
       return response.json();
     })
     .then((data) => {
-      console.log(data.products);
-      const html = data.products
+      //console.log(data);
+      const html = data.attributes
         .map((product) => {
           return `
             <div class="col-md-4">
@@ -20,6 +23,7 @@ function getProducts() {
                   <h4><b>${product.name}</b></h4>
                   <p>Precio: ${product.price}</p>
                   <p>Descuento: ${product.discount} %</p>
+                  <p class="text-info">Categoria: ${product.category.name}</p>
                 </div>
               </div>
             </div>
@@ -34,13 +38,79 @@ function getProducts() {
     });
 }
 
-function getProductsByCategory() {
-    
+function getCategories() {
+  return fetch(baseUrlCategories)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("Error");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const html = data
+      
+        .map((category) => {
+          
+          return `
+           
+            <option id="category" value="${category.id}">${category.name}</option>
+            
+          `;
+        })
+        .join("");
+      document.querySelector("#categoryDropDown").insertAdjacentHTML("beforeend",html)
+    });
+}
 
+function getSelectedOption() {
+  const select = document.getElementById("categoryDropDown");
+  select.addEventListener("change", function () {
+    const selectedOption = this.options[select.selectedIndex];
+    clearProducts();
+    getProductsByCategory(selectedOption.value);
+  });
+ 
+}
+
+function getProductsByCategory(selectedOption) {
+ 
+  return fetch(baseUrlProductsByCategory + `/${selectedOption}` + `/products`)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("Error");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const html = data.attributes
+      .map((product) => {
+        return `
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top img-fluid" src="${product.url_image}" alt="${product.name}" style="width:100%; height:30%;">
+              <div class="card-body">
+                <h4><b>${product.name}</b></h4>
+                <p>Precio: ${product.price}</p>
+                <p>Descuento: ${product.discount} %</p>
+                <p class="text-info">Categoria: ${product.category.name}</p>
+              </div>
+            </div>
+          </div>
+          
+          `;
+      })
+      .join("");
+    document.querySelector("#app").innerHTML = html;
+      
+    });
 }
 
 
-
+function clearProducts() {
+  document.querySelector("#app").innerHTML = "";
+}
 
 getProducts();
-getProductsByCategory();
+getCategories();
+getSelectedOption();
+

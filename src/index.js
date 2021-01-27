6d5f8d7ded46adde4154f71d8a7c745f9473c62f
Diego <diegof.df58@gmail.com>
2021-01-26 23:29:37 -0300

    workingonPagination

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const baseUrlProductsByCategory = "https://bsalebackend.herokuapp.com/categories
 
 const loader = document.querySelector("#loading");
 
-var loadingDiv = document.getElementById('loading');
+const loadingDiv = document.getElementById('loading');
 
 
 
@@ -22,24 +22,26 @@ function getProducts() {
   
   showSpinner();
   return fetch(baseURL)
-    
+
     .then((response) => {
       if (!response.ok) {
         throw Error("Error");
       }
       return response.json();
-      
-      
+
+
     })
     .then((data) => {
       //console.log(data);
-      
-      setTimeout(()=> {
+
+      setTimeout(() => {
+        
         const html = data.attributes
-        .map((product) => {
-          const name = product.name;
-          
-          return `
+          .map((product) => {
+            const name = product.name;
+            clearProducts();
+            
+            return `
             <div class="col-md-3 col-sm-6 animate__animated animate__backInDown animate__slow">
               <div class="card mb-4 box-shadow">
                 <img class="card-img-top img-fluid" src="${product.url_image}" alt="${product.name}" style="width:100%; height:30%;">
@@ -48,21 +50,21 @@ function getProducts() {
                   <p  class="card-text">${product.price}</p>
                   <p>Descuento: ${product.discount} %</p>
                   <p class="text-info">Categoria: ${product.category.name}</p>
-                  <a href="#" class="agregar-carrito btn btn-info" data-id="${product.id}"><i class="fa fa-cart-plus"></i>&nbsp;  AGREGAR AL CARRITO</a>
+                  <a href="#" class="agregar-carrito btn btn-outline-dark" data-id="${product.id}"><i class="agregar-carrito fas fa-shopping-cart"></i></a>
                 </div>
               </div>
             </div>
             
             `;
-        })
-        .join("");
+          })
+          .join("");
         document.querySelector("#app").innerHTML = html;
         hideSpinner();
       }, 1500);
-     
-        
-      
-      
+
+
+
+
     })
     .catch((error) => {
       console.log(error);
@@ -85,19 +87,19 @@ function getCategories() {
           `;
         })
         .join("");
-      document.querySelector("#categoryDropDown").insertAdjacentHTML("beforeend",html)
+      document.querySelector("#categoryDropDown").insertAdjacentHTML("beforeend", html)
     });
 }
 
 function getSelectedOption() {
-  
+
   const select = document.getElementById("categoryDropDown");
   select.addEventListener("change", function () {
     const selectedOption = this.options[select.selectedIndex];
     clearProducts();
     getProductsByCategory(selectedOption.value);
   });
- 
+
 }
 
 function getProductsByCategory(selectedOption) {
@@ -107,18 +109,16 @@ function getProductsByCategory(selectedOption) {
       if (!response.ok) {
         throw Error("Error");
       }
-      
+
       return response.json();
-     
+
     })
     .then((data) => {
-      
-      
-        const html = data.attributes
+      const html = data.attributes
         .map((product) => {
-          
+
           return `
-            <div class="col-md-3 col-sm-6 animate__animated animate__backInDown animate__slow">
+          <div class="col-md-3 col-sm-6 animate__animated animate__backInDown animate__slow">
             <div class="card mb-4 box-shadow">
               <img class="card-img-top img-fluid" src="${product.url_image}" alt="${product.name}" style="width:100%; height:30%;">
               <div class="card-body">
@@ -134,12 +134,94 @@ function getProductsByCategory(selectedOption) {
             `;
         })
         .join("");
-        document.querySelector("#app").innerHTML = html;
-        hideSpinner();
-     
-      
+      document.querySelector("#app").innerHTML = html;
+      hideSpinner();
+
+
     });
 }
+
+function getTotalPages(){
+  return fetch(baseURL)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("Error");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      let totalPage = 0;
+      const html = data.attributes
+      .map((product) => {
+       totalPage = totalPage + 1;
+        return `        
+        <a onclick="getProductsByPagination()" id="page-links" class="page-link" href="#" value="${totalPage}">${totalPage}</a>
+        `;
+      })
+      .join("");
+      document.querySelector("#pagination").innerHTML = html;
+      
+    });
+    
+}
+
+
+
+function getSelectedPagination(){
+ 
+ 
+  
+  clearProducts();
+  
+  
+}
+
+
+function getProductsByPagination(pageNumber) {
+  
+  return fetch(baseURL + `/page` + `/${1}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("Error");
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      setTimeout(() => {  
+        
+        const html = data.attributes
+          .map((product) => {
+            return `
+              <div class="col-md-3 col-sm-6 animate__animated animate__backInDown animate__slow">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top img-fluid" src="${product.url_image}" alt="${product.name}" style="width:100%; height:30%;">
+                <div class="card-body">
+                  <h4  class="card-title"><b>${product.name}</b></h4>
+                  <p  id="card-price" class="card-text">${product.price}</p>
+                  <p>Descuento: ${product.discount} %</p>
+                  <p class="text-info">Categoria: ${product.category.name}</p>
+                  <a href="#" class="agregar-carrito btn btn-info" data-id="${product.id}"><i class="fa fa-cart-plus"></i>&nbsp;  AGREGAR AL CARRITO</a>
+                </div>
+              </div>
+            </div>
+              
+            `;
+          })
+          .join("");
+        document.querySelector("#app").innerHTML = html;
+      hideSpinner();
+      }, 1500);
+    })
+}
+
+
+
+
+
+
+
+
 
 
 function clearProducts() {
@@ -153,7 +235,7 @@ function clearProducts() {
 
 
 
-
+getTotalPages()
 getProducts();
 getCategories();
 getSelectedOption();
